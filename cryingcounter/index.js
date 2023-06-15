6bad89294cb43c124783eeb8b20205557474e98d
Len Peel <135646327+lennypeel98@users.noreply.github.com>
2023-06-15 19:57:17 +0200

    Update index.js

--- cryingcounter/index.js
+++ cryingcounter/index.js
@@ -1,10 +1,14 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let count = 0
+let storedCount = localStorage.getItem('rtcount')
+let count = storedCount ? Number(storedCount) : 0
+countEl.textContent = count
 
 function increment() {
+    count = count + 1
     count += 1
     countEl.textContent = count
+    localStorage.setItem('rtcount', count)
 }
 
 function save() {
